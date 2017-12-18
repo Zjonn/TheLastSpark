fce2154ccf10bf21654567052ed5183f4b35d5ee
Zjonn <Zjonnn@gmail.com>
2017-12-18 18:25:21 +0100

    0.1
    Stworzyłem foldery, zaczynam pracę nad skryptem odpowiedzialnym za ruch

---
+++ The last spark/Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
