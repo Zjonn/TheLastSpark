5ccf03825ce8315c33a6ca4efd4e93da99208923
Zjonn <Zjonnn@gmail.com>
2017-12-24 18:27:59 +0100

    Obrót

---
+++ The last spark/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform toFollow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 v = toFollow.position;
+        v.z = transform.position.z;
+
+        //float dis = Vector3.Distance(v, transform.position);
+        //float lerp = dis/16 * Time.deltaTime * 8;
+        //if(dis > 20)
+        //{
+        //    lerp = 1;
+        //}
+        //else if(dis < 4)
+        //{
+        //    lerp = 0;
+        //}
+        this.transform.position = v;
+    }
+}
--- The last spark/Assets/Scripts/Movement.cs
+++ The last spark/Assets/Scripts/Movement.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
+    public float speed = 1;
 
-    Rigidbody2D rb;
+    List<Transform> shipEngineParts;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Vector3 mousePos;
+    private Rigidbody2D rb2d;
+
+    private bool wPressed = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        shipEngineParts = new List<Transform>();
+        Transform[] engineParts = GetComponentsInChildren<Transform>();
+
+        foreach (Transform t in engineParts)
+        {
+            if (t.tag.Equals("Engine"))
+                shipEngineParts.Add(t);
+        }
+    }
+
+
+    private void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        wPressed = Input.GetKey(KeyCode.W);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 dir = mousePos - this.transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+        angle = Mathf.LerpAngle(rb2d.rotation, angle, Time.deltaTime);
+        rb2d.MoveRotation(angle);
+
+        if (wPressed)
+            rb2d.AddForce(transform.up * speed);
+    }
 }
