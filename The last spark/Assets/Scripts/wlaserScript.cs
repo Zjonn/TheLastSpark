c0b4272c69197a93d0cc3f8f5c31b1f53f368baf
Grzegorz Bielecki <288388@uwr.edu.pl>
2018-01-07 03:19:26 +0100

    Dodałem działko proste laserowe.

---
+++ The last spark/Assets/Scripts/wlaserScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wlaserScript : MonoBehaviour
+{
+
+    public Object laser;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown ("space"))
+        {
+            FireLasert();
+        }
+    }
+
+    void FireLasert()
+    {
+        Instantiate(laser, transform.position + transform.up, transform.rotation);
+    }
+}
