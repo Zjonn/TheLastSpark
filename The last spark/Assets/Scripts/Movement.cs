6c3b5caf1c7ef044e691d60506f944b2a097e5cf
Zjonn <Zjonnn@gmail.com>
2018-01-07 01:17:40 +0100

    Ruch WASD + obrót

--- The last spark/Assets/Scripts/Movement.cs
+++ The last spark/Assets/Scripts/Movement.cs
@@ -6,30 +6,38 @@ public class Movement : MonoBehaviour
 {
     public float speed = 1;
 
-    List<Transform> shipEngineParts;
-
+    //Silniki odpowiadające za ruch w danym kierunku
     List<Transform> partsW;
     List<Transform> partsS;
     List<Transform> partsA;
     List<Transform> partsD;
 
+    //Silniki odpowiadające za obrót w danym kierunku
+    List<Transform> partsQ;
+    List<Transform> partsE;
+
     private Vector3 mousePos;
     private Rigidbody2D rb2d;
 
     private bool wPressed = false;
+    private bool aPressed = false;
+    private bool sPressed = false;
+    private bool dPressed = false;
 
     // Use this for initialization
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        shipEngineParts = new List<Transform>();
 
         partsW = new List<Transform>();
         partsS = new List<Transform>();
         partsA = new List<Transform>();
         partsD = new List<Transform>();
+
+        partsE = new List<Transform>();
+        partsQ = new List<Transform>();
+
         Transform[] engineParts = GetComponentsInChildren<Transform>();
-        
 
         foreach (Transform t in engineParts)
         {
@@ -37,7 +45,17 @@ public class Movement : MonoBehaviour
                 continue;
 
             if (t.rotation.Equals(transform.rotation))
+            {
                 partsW.Add(t);
+                if (t.position.x < transform.position.x)
+                {
+                    partsE.Add(t);
+                }
+                else
+                {
+                    partsQ.Add(t);
+                }
+            }
             else if (t.rotation.eulerAngles.Equals(new Vector3(0, 0, 180)))
                 partsS.Add(t);
             else if (t.rotation.eulerAngles.Equals(new Vector3(0, 0, 90)))
@@ -45,12 +63,10 @@ public class Movement : MonoBehaviour
             else if (t.rotation.eulerAngles.Equals(new Vector3(0, 0, 270)))
                 partsD.Add(t);
 
-            shipEngineParts.Add(t);
-            print(t.rotation.eulerAngles);
+
+            //print(t.rotation.eulerAngles);
         }
     }
-
-
 
     private void Update()
     {
@@ -60,18 +76,35 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 dir = mousePos - this.transform.position;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+        Vector3 shipDir = mousePos - this.transform.position;
+        float angle = Mathf.Atan2(shipDir.y, shipDir.x) * Mathf.Rad2Deg - 90;
         angle = Mathf.LerpAngle(rb2d.rotation, angle, Time.deltaTime);
-        rb2d.MoveRotation(angle);
 
         if (wPressed)
         {
-            foreach(Transform t in partsW)
-            {
-                rb2d.AddForce(new Vector2(speed, 0)); 
-            }
+            wasdAddForce(partsW);
         }
-            
+        if (aPressed)
+        {
+            wasdAddForce(partsA);
+        }
+        if (sPressed)
+        {
+            wasdAddForce(partsS);
+        }
+        if (dPressed)
+        {
+            wasdAddForce(partsD);
+        }
+
+    }
+
+    private void wasdAddForce(List<Transform> parts)
+    {
+        foreach(Transform t in parts)
+        {
+            Vector3 dir = t.rotation * new Vector3(0, speed, 0);
+            rb2d.AddForceAtPosition(new Vector2(dir.x, dir.y), t.position);
+        }
     }
 }
