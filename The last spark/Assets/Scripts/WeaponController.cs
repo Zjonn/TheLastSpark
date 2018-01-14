d28b2e47eeb24ddd519ce5b6b275fe8ec6473776
Grzegorz Bielecki <288388@uwr.edu.pl>
2018-01-14 05:25:12 +0100

    Dodałem kontroler obsługujący wiele broni jednocześnie.

---
+++ The last spark/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WeaponController : MonoBehaviour
+{
+
+    Transform[] objects;
+    List<Transform> WLaser;
+    List<Transform> WRocket;
+    List<Transform> WMelee;
+
+    // Use this for initialization
+    void Start()
+    {
+        WLaser = new List<Transform>();
+        WRocket = new List<Transform>();
+        WMelee = new List<Transform>();
+
+        objects = gameObject.GetComponentsInChildren<Transform>() as Transform[];
+        if (objects != null)
+        {
+            foreach (Transform object0 in objects)
+            {
+                if (object0.CompareTag("WeaponLaser"))
+                {
+                    WLaser.Add(object0);
+                }
+                else if (object0.CompareTag("WeaponLaser"))
+                {
+                    WRocket.Add(object0);
+                }
+                else if (object0.CompareTag("WeaponLaser"))
+                {
+                    WMelee.Add(object0);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && WLaser != null)
+        {
+            foreach (Transform weapon in WLaser)
+            {
+                ExecuteEvents.Execute<EventManager>(weapon.gameObject, null, (x, y) => x.Fire());
+
+            }
+        }
+
+    }
+}
--- The last spark/Assets/Scripts/wlaserScript.cs
+++ The last spark/Assets/Scripts/wlaserScript.cs
@@ -2,31 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class wlaserScript : MonoBehaviour
+public class wlaserScript : MonoBehaviour, EventManager
 {
 
     public GameObject laser;
     public Transform spawnPointLaser;
 
     private GameObject clone;
+    Rigidbody2D player;
 
     // Use this for initialization
     void Start()
     {
-
+        player = gameObject.GetComponentInParent(typeof(Rigidbody2D)) as Rigidbody2D;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown ("space"))
-        {
-            FireLasert();
-        }
-    }
 
-    void FireLasert()
+    public void FireLasert()
     {
         clone = Instantiate(laser, spawnPointLaser.position, spawnPointLaser.rotation) as GameObject;
+        if (player != null)
+            clone.GetComponent<Rigidbody2D>().velocity = player.velocity;
+         
+    }
+
+    public void Fire()
+    {
+        FireLasert();
     }
 }
