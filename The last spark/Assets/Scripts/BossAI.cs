c46ea625432cd09c979f88fa360e7d7b9e41b85e
Grzegorz Bielecki <288388@uwr.edu.pl>
2018-01-21 07:09:36 +0100

    Dodalem wczesną wersje bossa.

---
+++ The last spark/Assets/Scripts/BossAI.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : MonoBehaviour {
+
+    public Transform player;
+    Vector3 firstPos;
+    bool isAttack;
+    public float health;
+    float maxHealth;
+    float howFar;
+
+    public BossWeapon weapon0;
+    public BossWeapon weapon1;
+    public BossWeapon weapon2;
+    public BossWeapon weapon3;
+    public BossWeapon weapon4;
+    public BossWeapon weapon5;
+
+    List<BossWeapon> weapons;
+
+    public enum BossActionType
+    {
+        Idle,
+        Moving,
+        AvoidingObstacle,
+        Patrolling,
+        AttackingType0,
+        AttackingType1,
+        AttackingType2
+    }
+
+    private BossActionType eCurState = BossActionType.Idle;
+
+
+    // Use this for initialization
+    void Start () {
+        firstPos = transform.position;
+        maxHealth = health;
+        isAttack = false;
+        weapons = new List<BossWeapon> { weapon0, weapon1, weapon2, weapon3, weapon4, weapon5 };
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        howFar = Vector3.Distance(transform.position, player.position);
+
+        if (howFar > 50)
+        {
+            eCurState = BossActionType.Idle;
+            CancelInvoke();
+            isAttack = false;
+        }
+
+        switch (eCurState)
+        {
+            case BossActionType.Idle:
+                HandleIdleState();
+                break;
+
+            //case BossActionType.Moving:
+            //    HandleMovingState();
+            //    break;
+
+            //case BossActionType.AvoidingObstacle:
+            //    HandleAvoidingObstacleState();
+            //    break;
+
+            //case BossActionType.Patrolling:
+            //    HandlePatrollingState();
+            //    break;
+
+            case BossActionType.AttackingType0:
+                HandleAttackingType0State();
+                break;
+        }
+    }
+
+    void HandleIdleState()
+    {
+        InstaRegenerate();
+
+        if (howFar<50)
+        {
+            eCurState = BossActionType.AttackingType0;
+        }
+    }
+
+    void HandleAttackingType0State()
+    {
+        if (!isAttack)
+        {
+            InvokeRepeating("FireType1", 0.5f, 1f);
+            InvokeRepeating("Rotate", 0.5f, 1f);
+            isAttack = true;
+        }
+    }
+
+    void FireType1()
+    {
+        foreach(BossWeapon weapon in weapons)
+        {
+            weapon.BossFireType1();
+        }
+    }
+
+    void fireType0()
+    {
+        foreach (BossWeapon weapon in weapons)
+        {
+            weapon.BossFireType0();
+        }
+    }
+
+    void Rotate()
+    {
+
+    }
+
+    void InstaRegenerate()
+    {
+        health = maxHealth;
+    }
+
+}
---
+++ The last spark/Assets/Scripts/EnemyLaserScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLaserScript : MonoBehaviour, IDamageAmount
+{
+    // Use this for initialization
+
+    public float movementSpeed = 1;
+    public float damage = 10;
+    bool die = false;
+
+    void Start () {
+        Destroy(gameObject, 2);
+    }
+
+    void Update()
+    {
+        if (die)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        transform.Translate(Vector2.up * movementSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        die = true;
+    }
+
+    public float GetDamage()
+    {
+        return damage;
+    }
+}
