f166ef9566d4bf640216ed3536a8a7ebf002357d
Zjonn <Zjonnn@gmail.com>
2018-01-21 16:04:09 +0100

    Camera update

--- The last spark/Assets/Scripts/AsteroidDamageHandler.cs
+++ The last spark/Assets/Scripts/AsteroidDamageHandler.cs
@@ -9,42 +9,47 @@ public class AsteroidDamageHandler : MonoBehaviour
 
     float curDamage = 0;
     float damage = 0;
+    Color c;
 
     // Use this for initialization
     void Start()
     {
         spriteRend = GetComponent<SpriteRenderer>();
+        spriteRend.color = Color.HSVToRGB(Random.value, 1, 1);
         firstColor = spriteRend.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (damage>0)
+        if (damage > 0)
         {
             //Zmiana koloru przy otrzymaniu obrażeń
-            spriteRend.color = Color.Lerp(spriteRend.color, Color.green, Mathf.PingPong(Time.time, 0.1f));
+            spriteRend.color = Color.Lerp(spriteRend.color, c, Mathf.PingPong(Time.time, 0.1f));
 
             //Zmiana wielkości
             Vector3 velocity = Vector3.zero;
             transform.localScale = Vector3.SmoothDamp(transform.localScale, new Vector3(0, 0, 0), ref velocity, 0.3f);
-            damage -= curDamage * Time.deltaTime*5;
+            damage -= curDamage * Time.deltaTime * 5;
         }
         else
         {
-            spriteRend.color = Color.Lerp(spriteRend.color, firstColor, Mathf.PingPong(Time.time, 0.2f));
+            float H, S, V;
+           Color.RGBToHSV(firstColor, out H, out S, out V);
+            spriteRend.color = Color.Lerp(spriteRend.color, Color.HSVToRGB(H, 1 - transform.localScale.x / 20,1 ), Mathf.PingPong(Time.time, 0.2f));
         }
         if (transform.localScale.x < 0.5f)
         {
             Die();
         }
-        
+
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         damage += collision.GetComponent<IDamageAmount>().GetDamage();
         curDamage = damage;
+        c = Color.HSVToRGB(Random.value, 1, 1);
     }
 
     void Die()
